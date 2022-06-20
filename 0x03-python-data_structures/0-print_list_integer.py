7916ecd957b7e3c8acf61e0b4a1421c7ed0fc737
Emzykings <adeitanemmanuel086@gmail.com>
2022-06-20 12:05:13 -0700

    python-data-structure

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+
+        text = ""
+
+            for v in my_list[:]:
+
+                        text += "{:d}\n".format(v)
+
+                            print(text, end='')
