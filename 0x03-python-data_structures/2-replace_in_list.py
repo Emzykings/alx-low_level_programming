93de752376a8fbb2b238494a8cdcaf22bc21d1f5
EMMANUEL ADEITAN <99521436+Emzykings@users.noreply.github.com>
2022-06-21 11:49:43 +0100

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/2-replace_in_list.py
+++
@@ -1,9 +0,0 @@
-ce_in_list(my_list, idx, element):
-
-        if idx < 0 or idx >= len(my_list):
-
-                    return (my_list)
-
-                    my_list[idx] = element
-
-                        return (my_list)
--- 0x03-python-data_structures/8-multiple_returns.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def multiple_returns(sentence):
-
-        length = len(sentence)
-
-            return (length, (sentence[0] if length > 0 else None),)
